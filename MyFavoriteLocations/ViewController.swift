5cbcfab0905c7bb39c1f7fd0f255547d7f3757fb
Dan DeAngelis <daniel.deangelis98@gmail.com>
2018-06-12 13:15:03 -0400

    MyFavoriteLocations
    An app to find your favorite place

---
+++ MyFavoriteLocations/ViewController.swift
@@ -0,0 +1,97 @@
+//
+//  ViewController.swift
+//  MyFavoriteLocations
+//
+//  Created by Dan DeAngelis on 6/12/18.
+//  Copyright © 2018 deangelisLogic. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var TitleLabel: UILabel!
+    
+    @IBOutlet weak var actionSelector: UISegmentedControl!
+    
+    @IBOutlet weak var signInLabel: UILabel!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var SignInButton: UIButton!
+    
+    var userLogin:Bool = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func actionSelectorChanged(_ sender: UISegmentedControl) {
+        
+        userLogin = !userLogin //User has selected register button rather than sign in.
+        
+        //See what the user is doing
+        
+        if userLogin {
+            signInLabel.text = "Sign In"
+            SignInButton.setTitle("Sign In", for: .normal)
+        }
+        else {
+            signInLabel.text = "Register"
+            SignInButton.setTitle("Register", for: .normal)
+        }
+    }
+    
+    @IBAction func signInButtonPressed(_ sender: UIButton) {
+        
+        //check if sign in or register
+        // only if they have entered something in both fields
+        
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            
+            if userLogin {
+                //Sign in with Firebase
+                
+                Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+                    
+                    if let u = user {
+                        //User is found, go to map page
+                        self.performSegue(withIdentifier: "goToMainScreen", sender: self)
+                    }
+                    else {
+                        //Error, check error, show error message (user not found)
+                    }
+                }
+            }
+            else {
+                //register with Firebase
+                
+                Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+                    
+                    if let u = user {
+                        //User is found, go to map page
+                        self.performSegue(withIdentifier: "goToMainScreen", sender: self)
+                    }
+                    else {
+                        //Error, check error, show error message (user not found)
+                    }
+                }
+                
+                
+            }
+        }
+        
+
+
+    }
+}
+
